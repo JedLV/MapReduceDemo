6d49b02b56c6d6fd9997221d4fdd5a79c5134fa0
编程 <1660873558@qq.com>
2021-09-11 21:38:38 +0800

    Hadoop 区内排序

---
+++ src/main/java/com/atjedmogul/mapreduce/partitionercompable/FlowDriver2.java
@@ -0,0 +1,54 @@
+package com.atjedmogul.mapreduce.partitionercompable;
+
+import com.atjedmogul.mapreduce.partitioner.ProvincePartitioner;
+import com.atjedmogul.mapreduce.writable.FlowBean;
+import com.atjedmogul.mapreduce.writable.FlowMapper;
+import com.atjedmogul.mapreduce.writable.FlowReducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class FlowDriver2 {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        //1 获取job对象
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+
+        //2 关联本Driver类
+        job.setJarByClass(FlowDriver2.class);
+
+        //3 关联Mapper和Reducer
+        job.setMapperClass(FlowMapper.class);
+        job.setReducerClass(FlowReducer.class);
+
+        //4 设置Map端输出数据的KV类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(FlowBean.class);
+
+        //5 设置程序最终输出的KV类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+
+        //8 指定自定义分区器
+        job.setPartitionerClass(ProvincePartitioner.class);
+
+        //9 同时指定相应数量的ReduceTask
+        job.setNumReduceTasks(5);
+
+        //6 设置输入输出路径
+        FileInputFormat.setInputPaths(job, new Path("F:\\11_input\\inputflow"));
+        FileOutputFormat.setOutputPath(job, new Path("F:\\11_output\\partitionout"));
+
+        //7 提交Job
+        boolean b = job.waitForCompletion(true);
+        System.exit(b ? 0 : 1);
+    }
+}
+
